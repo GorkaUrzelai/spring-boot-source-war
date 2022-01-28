e96d30498d6dc4d38a2207964417fa6511c1ec72
GorkaUrzelai <gorka.urzelai@alumni.mondragon.edu>
2022-01-28 14:02:19 +0100

    organize imports

--- src/main/java/com/gurzelai/reparationscalendar/ReparationscalendarApplication.java
+++ src/main/java/com/gurzelai/reparationscalendar/ReparationscalendarApplication.java
@@ -3,10 +3,6 @@ package com.gurzelai.reparationscalendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
 public class ReparationscalendarApplication {
 
--- src/main/java/com/gurzelai/reparationscalendar/controllers/DemoController.java
+++ src/main/java/com/gurzelai/reparationscalendar/controllers/DemoController.java
@@ -1,9 +1,8 @@
 package com.gurzelai.reparationscalendar.controllers;
 
-import com.gurzelai.reparationscalendar.repositories.CustomerRepository;
 import com.gurzelai.reparationscalendar.model.Customer;
+import com.gurzelai.reparationscalendar.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 public class DemoController {
